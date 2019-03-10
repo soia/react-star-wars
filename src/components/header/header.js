6375e1db9c6dc4736ae905c2147bab340f31e6a1
soia <nikita.soya@gmail.com>
2019-03-10 23:00:27 +0200

    work with server

---
+++ src/components/header/header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import './header.css';
+
+const Header = () => {
+  return (
+    <div className="header d-flex">
+      <h3>
+        <a href="1">
+          Star DB
+        </a>
+      </h3>
+      <ul className="d-flex">
+        <li>
+          <a href="1">People</a>
+        </li>
+        <li>
+          <a href="1">Planets</a>
+        </li>
+        <li>
+          <a href="1">Starships</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Header;
